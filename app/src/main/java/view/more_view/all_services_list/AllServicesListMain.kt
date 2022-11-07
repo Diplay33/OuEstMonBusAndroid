ce132e100a87773d0993f812511bbdcc06c26fc9
Jacques HU <jacques33185@gmail.com>
2022-11-07 15:34:27 +0100

    - created AllServicesListRow skeleton

--- app/src/main/java/view/more_view/all_services_list/AllServicesListMain.kt
+++ app/src/main/java/view/more_view/all_services_list/AllServicesListMain.kt
@@ -1,5 +1,6 @@
 package view.more_view.all_services_list
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
@@ -7,6 +8,10 @@ import androidx.navigation.NavController
 @Composable
 fun AllServicesListMain(navController: NavController) {
     Scaffold(topBar = { AllServicesListTopBar(navController) }) {
-        
+        LazyColumn {
+            item {
+                AllServicesListRow()
+            }
+        }
     }
 }
---
+++ app/src/main/java/view/more_view/all_services_list/AllServicesListRow.kt
@@ -0,0 +1,112 @@
+package view.more_view.all_services_list
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ouestmonbus.R
+import model.DTO.Line
+import model.DTO.Lines
+import model.DTO.ListGroup
+
+@Composable
+fun AllServicesListRow() {
+    val exampleLine = Line(3, "Lianes 3", R.drawable.lianes_3, R.color.lianes, ListGroup.LIANES)
+    //val exampleService =
+
+    Row(modifier = Modifier
+        .padding(horizontal = 15.dp)
+        .fillMaxWidth()
+    ) {
+        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
+            .background(
+                color = colorResource(exampleLine.lineColorResource).copy(alpha = 0.2f),
+                shape = RoundedCornerShape(10.dp)
+            )
+            .padding(horizontal = 15.dp)
+            .padding(top = 7.dp, bottom = 5.dp)
+            .fillMaxWidth()
+        ) {
+            Column {
+                Row {
+                    Image(
+                        painter = painterResource(id = exampleLine.lineImageResource),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(25.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+
+                    Spacer(modifier = Modifier
+                        .width(10.dp)
+                    )
+
+                    Text(
+                        text = exampleLine.lineName,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                    )
+                }
+
+                Row {
+                    Icon(
+                        imageVector = Icons.Rounded.PlayArrow,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .offset(x = -7.dp)
+                    )
+
+                    Column(verticalArrangement = Arrangement.spacedBy(0.dp)) {
+                        Text(
+                            text = "BORDEAUX",
+                            fontSize = 13.sp,
+                            color = Color.Gray,
+                            modifier = Modifier
+                                .offset(y = 2.dp)
+                        )
+
+                        Text("Quinconces", fontSize = 18.sp, modifier = Modifier
+                            .offset(y = -2.dp)
+                        )
+                    }
+                }
+            }
+
+            Row(modifier = Modifier
+                .align(Alignment.CenterVertically)
+            ) {
+                Text("1415", fontSize = 18.sp, fontWeight = FontWeight.Bold, modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                )
+
+                Spacer(modifier = Modifier
+                    .width(7.dp)
+                )
+
+                Icon(
+                    imageVector = Icons.Rounded.ArrowForward,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                )
+            }
+        }
+    }
+}
