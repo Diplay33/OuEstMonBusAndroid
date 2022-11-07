c3f884c0a98a6c7905243bd40140c93a93587350
Jacques HU <jacques33185@gmail.com>
2022-11-08 00:03:36 +0100

    - added data to VehicleData and Destination

--- app/src/main/java/model/DAO/AccessData/DestinationData.kt
+++ app/src/main/java/model/DAO/AccessData/DestinationData.kt
@@ -3,6 +3,10 @@ package model.DAO.AccessData
 class DestinationData {
     companion object {
         val destinations: Map<String, List<String>> = mapOf(
+            //5
+            "5 Chemins" to listOf("LE HAILLAN", "5 Chemins"),
+            //C
+            "Carle Vernet" to listOf("BORDEAUX", "Carle Vernet"),
             //Q
             "Quinconces" to listOf("BORDEAUX", "Quinconces"),
             //V
--- app/src/main/java/model/DAO/AccessData/VehicleData.kt
+++ app/src/main/java/model/DAO/AccessData/VehicleData.kt
@@ -7,11 +7,37 @@ class VehicleData {
             //ALSTOM Citadis TGA402
             mapOf("id" to "2201", "parkId" to "2201", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
             mapOf("id" to "2202", "parkId" to "2202", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2203", "parkId" to "2203", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2204", "parkId" to "2204", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2205", "parkId" to "2205", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2206", "parkId" to "2206", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2207", "parkId" to "2207", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2208", "parkId" to "2208", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2209", "parkId" to "2209", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2210", "parkId" to "2210", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2211", "parkId" to "2211", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2212", "parkId" to "2212", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2213", "parkId" to "2213", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2214", "parkId" to "2214", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2215", "parkId" to "2215", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2216", "parkId" to "2216", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2217", "parkId" to "2217", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2218", "parkId" to "2218", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2219", "parkId" to "2219", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2220", "parkId" to "2220", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "1301", "parkId" to "1301", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
 
             //ALSTOM Citadis TGA302
             mapOf("id" to "2241", "parkId" to "2241", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA302", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
             mapOf("id" to "2245", "parkId" to "2245", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA302", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
             mapOf("id" to "2544", "parkId" to "2544", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA302", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+
+            //HeuliezBus Access'BUS GX 327 CNG
+            mapOf("id" to "2554", "parkId" to "2554", "brand" to "HeuliezBus", "model" to "HeuliezBus Access'BUS GX 327 CNG", "type" to "BUS", "operator" to "Keolis Bordeaux Métropole"),
+
+            /*Keolis Gironde / Cars de Bordeaux*/
+            //HeuliezBus GX 337
+            mapOf("id" to "99115", "parkId" to "159115", "brand" to "HeuliezBus", "model" to "HeuliezBus GX 337", "type" to "BUS", "operator" to "Keolis Gironde/Cars de Bordeaux"),
         )
     }
 }
