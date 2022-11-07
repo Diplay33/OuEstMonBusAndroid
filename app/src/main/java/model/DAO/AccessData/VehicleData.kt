254a9b920d77892a394f75960ed0ce43a7bd1d36
Jacques HU <jacques33185@gmail.com>
2022-11-07 16:58:10 +0100

    - began adding data to VehicleData

---
+++ app/src/main/java/model/DAO/AccessData/VehicleData.kt
@@ -0,0 +1,12 @@
+package model.DAO.AccessData
+
+class VehicleData {
+    companion object {
+        val vehicles: List<Map<String, String>> = listOf(
+            /*Keolis Bordeaux Métropole*/
+            //ALSTOM Citadis TGA402
+            mapOf("id" to "2201", "parkId" to "2201", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+            mapOf("id" to "2202", "parkId" to "2202", "brand" to "ALSTOM", "model" to "ALSTOM Citadis TGA402", "type" to "TRAMWAY", "operator" to "Keolis Bordeaux Métropole"),
+        )
+    }
+}
