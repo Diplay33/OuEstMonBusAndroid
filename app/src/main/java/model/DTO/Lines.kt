0740b5e0df8b7adcec94b698cdc49b066b9af692
Jacques HU <jacques.hu@laddition.com>
2022-11-22 13:13:14 +0100

    - optimised navette tram getter

--- app/src/main/java/model/DTO/Lines.kt
+++ app/src/main/java/model/DTO/Lines.kt
@@ -60,28 +60,9 @@ class Lines {
                 if(line.id.toString() == lineId) {
                     return line
                 }
-                else if(lineId?.toInt() == 132 || lineId?.toInt() == 133 || lineId?.toInt() == 134
-                    || lineId?.toInt() == 135 || lineId?.toInt() == 136 || lineId?.toInt() == 137
-                    || lineId?.toInt() == 138 || lineId?.toInt() == 139 || lineId?.toInt() == 140
-                    || lineId?.toInt() == 141 || lineId?.toInt() == 142 || lineId?.toInt() == 143
-                    || lineId?.toInt() == 144 || lineId?.toInt() == 145 || lineId?.toInt() == 146
-                    || lineId?.toInt() == 147 || lineId?.toInt() == 148 || lineId?.toInt() == 149
-                    || lineId?.toInt() == 150 || lineId?.toInt() == 151 || lineId?.toInt() == 152
-                    || lineId?.toInt() == 153 || lineId?.toInt() == 154 || lineId?.toInt() == 155
-                    || lineId?.toInt() == 156 || lineId?.toInt() == 157 || lineId?.toInt() == 158
-                    || lineId?.toInt() == 159 || lineId?.toInt() == 160 || lineId?.toInt() == 161
-                    || lineId?.toInt() == 162 || lineId?.toInt() == 163 || lineId?.toInt() == 164
-                    || lineId?.toInt() == 166 || lineId?.toInt() == 167 || lineId?.toInt() == 168
-                    || lineId?.toInt() == 169 || lineId?.toInt() == 170 || lineId?.toInt() == 171
-                    || lineId?.toInt() == 172 || lineId?.toInt() == 173 || lineId?.toInt() == 174
-                    || lineId?.toInt() == 175 || lineId?.toInt() == 176 || lineId?.toInt() == 177
-                    || lineId?.toInt() == 178 || lineId?.toInt() == 179 || lineId?.toInt() == 180
-                    || lineId?.toInt() == 181 || lineId?.toInt() == 182 || lineId?.toInt() == 186
-                    || lineId?.toInt() == 187 || lineId?.toInt() == 188 || lineId?.toInt() == 189
-                    || lineId?.toInt() == 190 || lineId?.toInt() == 191 || lineId?.toInt() == 192
-                    || lineId?.toInt() == 193 || lineId?.toInt() == 194 || lineId?.toInt() == 195
-                    || lineId?.toInt() == 196 || lineId?.toInt() == 197 || lineId?.toInt() == 198
-                    || lineId?.toInt() == 199) {
+                else if((lineId?.toInt() in 132..164) ||
+                    (lineId?.toInt() in 166..182) ||
+                    (lineId?.toInt() in 186..199)) {
                     return Line(
                         id = 132,
                         lineName = "Navette Tram",
--- app/src/main/java/model/DTO/Services.kt
+++ app/src/main/java/model/DTO/Services.kt
@@ -22,28 +22,9 @@ class Services {
             ServiceDAO.getAllServices { services ->
                 val navetteTramServices = ArrayList<Service>()
                 services.forEach { service ->
-                    if(service.lineId == 132 || service.lineId == 133 || service.lineId == 134 ||
-                        service.lineId == 135 || service.lineId == 136 || service.lineId == 137 ||
-                        service.lineId == 138 || service.lineId == 139 || service.lineId == 140 ||
-                        service.lineId == 141 || service.lineId == 142 || service.lineId == 143 ||
-                        service.lineId == 144 || service.lineId == 145 || service.lineId == 146 ||
-                        service.lineId == 147 || service.lineId == 148 || service.lineId == 149 ||
-                        service.lineId == 150 || service.lineId == 151 || service.lineId == 152 ||
-                        service.lineId == 153 || service.lineId == 154 || service.lineId == 155 ||
-                        service.lineId == 156 || service.lineId == 157 || service.lineId == 158 ||
-                        service.lineId == 159 || service.lineId == 160 || service.lineId == 161 ||
-                        service.lineId == 162 || service.lineId == 163 || service.lineId == 164 ||
-                        service.lineId == 166 || service.lineId == 167 || service.lineId == 168 ||
-                        service.lineId == 169 || service.lineId == 170 || service.lineId == 171 ||
-                        service.lineId == 172 || service.lineId == 173 || service.lineId == 174 ||
-                        service.lineId == 175 || service.lineId == 176 || service.lineId == 177 ||
-                        service.lineId == 178 || service.lineId == 179 || service.lineId == 180 ||
-                        service.lineId == 181 || service.lineId == 182 || service.lineId == 186 ||
-                        service.lineId == 187 || service.lineId == 188 || service.lineId == 189 ||
-                        service.lineId == 190 || service.lineId == 191 || service.lineId == 192 ||
-                        service.lineId == 193 || service.lineId == 194 || service.lineId == 195 ||
-                        service.lineId == 196 || service.lineId == 197 || service.lineId == 198 ||
-                        service.lineId == 199) {
+                    if((service.lineId in 132..164) ||
+                        (service.lineId in 166..182) ||
+                        (service.lineId in 186..199)) {
                         navetteTramServices.add(service)
                     }
                 }
