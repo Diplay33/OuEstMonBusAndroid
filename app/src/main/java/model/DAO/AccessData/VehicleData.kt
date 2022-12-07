72db3095db8489c4abbc9b0ebe8b62dac05024a1
Jacques HU <jacques.hu@laddition.com>
2022-12-07 12:36:13 +0100

    - updated data for Citaro C2 NGT

--- app/src/main/java/model/DAO/AccessData/VehicleData.kt
+++ app/src/main/java/model/DAO/AccessData/VehicleData.kt
@@ -708,7 +708,7 @@ class VehicleData {
             mapOf("id" to "99045", "parkId" to "069045", "brand" to "MercedesBenz", "model" to "MercedesBenz Citaro C1", "type" to "BUS", "operator" to "Keolis Gironde/Cars de Bordeaux"),
 
             //MercedesBenz Citaro C2 NGT
-            mapOf("id" to "xxxx", "parkId" to "169xxx", "brand" to "MercedesBenz", "model" to "MercedesBenz Citaro C2 NGT", "type" to "BUS", "operator" to "Keolis Gironde/Cars de Bordeaux"),
+            mapOf("id" to "79069", "parkId" to "169069", "brand" to "MercedesBenz", "model" to "MercedesBenz Citaro C2 NGT", "type" to "BUS", "operator" to "Keolis Gironde/Cars de Bordeaux"),
 
 
             /*TRANSDEV Citram Aquitaine*/
