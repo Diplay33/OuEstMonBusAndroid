dac82fe45ba69eabe9d228b89d2a685277276b3a
Jacques HU <jacques33185@gmail.com>
2022-12-08 18:13:14 +0100

    - now displays nextSchedules from favorite stops into NextSchedulesHome view

--- app/src/main/java/view/next_schedules/NextSchedulesHomeFavoriteRow.kt
+++ app/src/main/java/view/next_schedules/NextSchedulesHomeFavoriteRow.kt
@@ -1,18 +1,131 @@
 package view.next_schedules
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 import model.DTO.Line
+import model.DTO.NextSchedule
+import model.DTO.NextSchedules
 import model.DTO.Station
+import view.next_schedules.search_line.search_stop_list.next_line_schedules.NextLineSchedulesView
 
 @Composable
 fun NextSchedulesHomeFavoriteRow(station: Station, lines: List<Line>) {
-    Row {
-        Text(text = station.name)
-        
-        lines.forEach { line ->  
-            Text(text = line.lineName)
+    val nextSchedules = remember {
+        mutableStateListOf<NextSchedule>()
+    }
+
+    LaunchedEffect(station) {
+        while(true) {
+            NextSchedules.getNextSchedulesByStationId(station.stationId) { values ->
+                nextSchedules.clear()
+                nextSchedules.addAll(values)
+            }
+
+            delay(10000)
+        }
+    }
+
+    Column(modifier = Modifier
+        .padding(horizontal = 15.dp)
+        .padding(bottom = 10.dp)
+        .fillMaxWidth()
+        .background(
+            Color(0xffF5F5F5),
+            shape = RoundedCornerShape(10.dp)
+        )
+    ) {
+        Column(modifier = Modifier
+            .padding(vertical = 10.dp)
+        ) {
+            Row(modifier = Modifier
+                .padding(horizontal = 15.dp)
+                .fillMaxWidth()
+            ) {
+                Text(
+                    text = station.name,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                )
+            }
+
+            Column {
+                lines.forEach { line ->
+                    Column(modifier = Modifier
+                        .padding(horizontal = 15.dp)
+                        .padding(top = 10.dp)
+                        .fillMaxWidth()
+                        .background(
+                            Color.White,
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                        .background(
+                            colorResource(id = line.lineColorResource).copy(alpha = 0.2f),
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                    ) {
+                        Row(modifier = Modifier
+                            .padding(horizontal = 15.dp)
+                        ) {
+                            Image(
+                                painter = painterResource(id = line.lineImageResource),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(40.dp)
+                            )
+
+                            Spacer(modifier = Modifier
+                                .width(15.dp)
+                            )
+
+                            Text(
+                                text = line.lineName,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .align(Alignment.CenterVertically)
+                            )
+                        }
+
+                        Row(modifier = Modifier
+                            .fillMaxWidth()
+                            .background(
+                                colorResource(id = line.lineColorResource).copy(alpha = 0.2f),
+                                shape = RoundedCornerShape(bottomStart = 10.dp, bottomEnd = 10.dp)
+                            )
+                        ) {
+                            Column {
+                                Spacer(modifier = Modifier
+                                    .height(15.dp)
+                                )
+
+                                NextSchedulesHomeFavoriteView(line, nextSchedules)
+
+                                Spacer(modifier = Modifier
+                                    .height(15.dp)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/view/next_schedules/NextSchedulesHomeFavoriteView.kt
@@ -0,0 +1,148 @@
+package view.next_schedules
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ouestmonbus.R
+import model.DTO.Line
+import model.DTO.NextSchedule
+import model.DTO.NextSchedulesDestinations
+
+@Composable
+fun NextSchedulesHomeFavoriteView(line: Line, nextSchedules: List<NextSchedule>) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 15.dp)
+        .background(
+            Color.White,
+            shape = RoundedCornerShape(10.dp)
+        )
+        .background(
+            colorResource(id = line.lineColorResource).copy(alpha = 0.2f),
+            shape = RoundedCornerShape(10.dp)
+        )
+    ) {
+        nextSchedules.forEach { nextSchedule ->
+            val destination = NextSchedulesDestinations.getDestinationFromRaw((nextSchedule.destination))
+            val displayedTime = nextSchedule.getEstimatedTimeLeft()
+
+            if(nextSchedule.lineId == line.id) {
+                Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 15.dp)
+                ) {
+                    Row {
+                        Icon(
+                            imageVector = Icons.Rounded.PlayArrow,
+                            contentDescription = null,
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .offset(x = (-7).dp)
+                        )
+
+                        if(destination.isEmpty()) {
+                            Text(text = nextSchedule.destination, fontSize = 18.sp, modifier = Modifier
+                                .padding(vertical = 8.dp)
+                                .align(Alignment.CenterVertically)
+                            )
+                        }
+                        else {
+                            Column(modifier = Modifier
+                                .padding(vertical = 3.dp)
+                            ) {
+                                Text(
+                                    text = destination.first(),
+                                    fontSize = 13.sp,
+                                    color = Color.Gray,
+                                    modifier = Modifier
+                                        .offset(y = 2.dp)
+                                )
+
+                                Text(destination.last(), fontSize = 18.sp, modifier = Modifier
+                                    .offset(y = (-2).dp)
+                                )
+                            }
+                        }
+                    }
+
+                    Box(contentAlignment = Alignment.Center, modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .size(35.dp)
+                        .background(
+                            colorResource(id = line.lineColorResource).copy(alpha = 0.2f),
+                            shape = RoundedCornerShape(10.dp)
+                        )
+                    ) {
+                        if(displayedTime == "0") {
+                            Text(
+                                text = "PROCHE",
+                                fontSize = 8.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        else {
+                            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+                                .fillMaxWidth()
+                            ) {
+                                Text(
+                                    text = displayedTime,
+                                    fontSize = 15.sp
+                                )
+
+                                Text(
+                                    text = "MIN",
+                                    fontSize = 10.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier
+                                        .offset(y = (-3).dp)
+                                )
+                            }
+                        }
+
+                        if(nextSchedule.isOnline) {
+                            Image(
+                                painter = painterResource(id = R.drawable.bean_small),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(15.dp)
+                                    .offset(x = 11.dp, y = (-11).dp)
+                                    //.alpha(indicatorOpacity)
+                            )
+                        }
+                    }
+                }
+
+                if(nextSchedule != nextSchedules.last { it.lineId == line.id }) {
+                    Row {
+                        Spacer(modifier = Modifier
+                            .width(39.dp)
+                        )
+
+                        Box(modifier = Modifier
+                            .clip(RectangleShape)
+                            .background(Color.LightGray)
+                            .fillMaxWidth()
+                            .height(1.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
