34f9d59fd7582a446ceb40bfb303d65577eb3d15
Jacques HU <jacques33185@gmail.com>
2022-12-13 13:35:21 +0100

    - now gets only the principal path of each line, when retrieving them

--- app/src/main/java/model/DAO/PathDAO.kt
+++ app/src/main/java/model/DAO/PathDAO.kt
@@ -7,7 +7,7 @@ import org.json.JSONObject
 class PathDAO {
     companion object {
         fun getPathsByLine(lineId: Int, callback: (List<Path>) -> Unit) {
-            CallAPI.run("https://data.bordeaux-metropole.fr/geojson/features/sv_chem_l?key=0234ABEFGH&filter={\"rs_sv_ligne_a\":$lineId}&attributes=[\"gid\",\"libelle\",\"sens\"]") { responseBody ->
+            CallAPI.run("https://data.bordeaux-metropole.fr/geojson/features/sv_chem_l?key=0234ABEFGH&filter={\"rs_sv_ligne_a\":$lineId,\"principal\":true}&attributes=[\"gid\",\"libelle\",\"sens\"]") { responseBody ->
                 val paths: MutableList<Path> = mutableListOf()
                 val welcomeJSONObject = JSONObject(responseBody)
                 val featuresJSONArray = welcomeJSONObject.getJSONArray("features")
