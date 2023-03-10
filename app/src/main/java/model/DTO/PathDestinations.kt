16bcecfe99df27f8c41e6f677787f6b2be33ce9d
Jacques HU <jacques33185@gmail.com>
2023-03-10 10:45:20 +0100

    - finished adding path destination data

--- app/src/main/java/model/DTO/PathDestinations.kt
+++ app/src/main/java/model/DTO/PathDestinations.kt
@@ -17,8 +17,11 @@ class PathDestinations {
                 lineId == 10 && finalString == "Hippodrome" -> {
                     return listOf("EYSINES", "Hippodrome", "")
                 }
-                lineId == 13 && finalString == "Les Pins" -> {
+                lineId == 13 || lineId == 28 || lineId == 32 && finalString == "Les Pins" -> {
                     return listOf("MERIGNAC", "Les Pins", "")
+                }
+                lineId == 105 || lineId == 111 && finalString == "Lycée Sud Médoc" -> {
+                    return listOf("LE TAILLAN", "Lycée Sud Médoc", "")
                 }
                 else -> {
                     PathDestinationData.destinations[finalString]?.let { values ->
