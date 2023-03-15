ae4ecaeb23bc98960bede327df32122db2cfadf3
Jacques HU <jacques33185@gmail.com>
2023-03-15 22:32:01 +0100

    - added loading view into schedules view

--- app/src/main/java/view/next_schedules/search_line/search_stop_list/next_line_schedules/line_schedules/LineSchedulesMain.kt
+++ app/src/main/java/view/next_schedules/search_line/search_stop_list/next_line_schedules/line_schedules/LineSchedulesMain.kt
@@ -1,18 +1,18 @@
 package view.next_schedules.search_line.search_stop_list.next_line_schedules.line_schedules
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import model.DTO.*
 import java.util.*
@@ -36,6 +36,9 @@ fun LineSchedulesMain(
     val collapsedGroupHandler: MutableList<Boolean> = sortedSchedules.map {
         false
     }.toMutableList()
+    val isLoading = remember {
+        mutableStateOf(true)
+    }
 
     LaunchedEffect(stationId) {
         Paths.getOrderedAllPathsByLine(lineId?.toInt() ?: 0) { returnedPaths ->
@@ -46,6 +49,7 @@ fun LineSchedulesMain(
 
                     Schedules.getSchedulesByStationAndPaths(stationId ?: "", value) {
                         schedules.addAll(it)
+                        isLoading.value = false
                     }
                 }
             }
@@ -55,22 +59,46 @@ fun LineSchedulesMain(
     Scaffold(
         topBar = { LineSchedulesTopBar(navController, stationName ?: "Arrêt inconnu") }
     ) { padding ->
-        LazyColumn(modifier = Modifier
-            .padding(padding)
-        ) {
-            items(sortedSchedules.size) {groupIndex ->
-                LineSchedulesGroup(
-                    schedules = sortedSchedules[groupIndex],
-                    line = line,
-                    paths = paths,
-                    collapsedGroupHandler = collapsedGroupHandler,
-                    groupIndex = groupIndex
+        if(isLoading.value) {
+            Column(verticalArrangement = Arrangement.Center, modifier = Modifier
+                .fillMaxSize()
+            ) {
+                CircularProgressIndicator(modifier = Modifier
+                    .size(25.dp)
+                    .align(Alignment.CenterHorizontally)
                 )
 
-                if(sortedSchedules[groupIndex] != sortedSchedules.last()) {
-                    Spacer(modifier = Modifier
-                        .height(30.dp)
+                Spacer(modifier = Modifier
+                    .height(10.dp)
+                )
+
+                Text(
+                    text = "Chargement en cours...",
+                    fontSize = 18.sp,
+                    color = Color.Gray,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
+        }
+        else {
+            LazyColumn(modifier = Modifier
+                .padding(padding)
+            ) {
+                items(sortedSchedules.size) {groupIndex ->
+                    LineSchedulesGroup(
+                        schedules = sortedSchedules[groupIndex],
+                        line = line,
+                        paths = paths,
+                        collapsedGroupHandler = collapsedGroupHandler,
+                        groupIndex = groupIndex
                     )
+
+                    if(sortedSchedules[groupIndex] != sortedSchedules.last()) {
+                        Spacer(modifier = Modifier
+                            .height(30.dp)
+                        )
+                    }
                 }
             }
         }
