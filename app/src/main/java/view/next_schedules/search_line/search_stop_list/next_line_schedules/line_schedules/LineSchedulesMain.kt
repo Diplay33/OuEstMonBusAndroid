3a7853e16f66cf20ca7fb5cc6d8b0ba726a90e2d
Jacques HU <jacques33185@gmail.com>
2023-03-19 15:39:52 +0100

    - created display more schedules button

--- app/src/main/java/view/next_schedules/search_line/search_stop_list/next_line_schedules/line_schedules/LineSchedulesMain.kt
+++ app/src/main/java/view/next_schedules/search_line/search_stop_list/next_line_schedules/line_schedules/LineSchedulesMain.kt
@@ -1,18 +1,31 @@
 package view.next_schedules.search_line.search_stop_list.next_line_schedules.line_schedules
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.diplay.ouestmonbus.R
 import model.DTO.*
+import view.Screens.ProchainsScreens
 
 @Composable
 fun LineSchedulesMain(
@@ -42,6 +55,7 @@ fun LineSchedulesMain(
     val isLoading = remember {
         mutableStateOf(true)
     }
+    val colorScheme = !isSystemInDarkTheme()
 
     LaunchedEffect(stationId) {
         Paths.getOrderedAllPathsByLine(lineId?.toInt() ?: 0) { returnedPaths ->
@@ -109,6 +123,52 @@ fun LineSchedulesMain(
                 LazyColumn(modifier = Modifier
                     .padding(padding)
                 ) {
+                    item {
+                        Row(
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 15.dp)
+                                .background(
+                                    if (colorScheme) Color.Transparent else Color(0xff18191A),
+                                    shape = RoundedCornerShape(10.dp)
+                                )
+                                .background(
+                                    colorResource(id = line.lineColorResource).copy(alpha = 0.2f),
+                                    shape = RoundedCornerShape(10.dp)
+                                )
+                                .height(45.dp)
+                                .clickable {
+                                    println("Display more schedules")
+                                }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Rounded.ArrowForward,
+                                contentDescription = null,
+                                tint = colorResource(id = line.lineColorResource),
+                                modifier = Modifier
+                                    .size(25.dp)
+                                    .rotate(-90f)
+                            )
+
+                            Spacer(modifier = Modifier
+                                .width(10.dp)
+                            )
+
+                            Text(
+                                text = "Afficher les résultats précédents",
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 18.sp,
+                                color = colorResource(id = line.lineColorResource)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier
+                            .height(30.dp)
+                        )
+                    }
+
                     items(if (displayMoreSchedules.value)
                         sortedSchedules.size
                     else
