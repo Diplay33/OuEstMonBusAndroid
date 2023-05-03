643808cf41946ffa715166b0b9b0ae2eb35af622
Jacques HU <jacques.hu@laddition.com>
2023-05-03 11:07:10 +0200

    - added modified destination

--- app/src/main/java/model/DAO/AccessData/NextSchedulesDestinationData.kt
+++ app/src/main/java/model/DAO/AccessData/NextSchedulesDestinationData.kt
@@ -61,6 +61,7 @@ class NextSchedulesDestinationData {
             "Gare de Bruges" to listOf("BRUGES", "Gare"),
             "Gare Routière" to listOf("SAINT MÉDARD EN JALLES", "Gare Routière"),
             "Gare St Jean" to listOf("BORDEAUX", "Gare Saint Jean"),
+            "Gare St Jean (arrêt de Descente)" to listOf("BORDEAUX", "Gare Saint Jean"),
             "G. Fauré" to listOf("BORDEAUX", "Gabriel Fauré"),
             "Gradignan Centre" to listOf("GRADIGNAN", "Centre"),
             "Grand Parc" to listOf("BORDEAUX", "Grand Parc"),
