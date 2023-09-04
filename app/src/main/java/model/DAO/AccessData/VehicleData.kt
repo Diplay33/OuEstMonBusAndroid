1efc865ffbe2f747958510179499bbbd01fc5ccd
Jacques HU <jacques33185@gmail.com>
2023-09-04 06:20:51 +0200

    - added latest citaros

--- app/src/main/java/model/DAO/AccessData/VehicleData.kt
+++ app/src/main/java/model/DAO/AccessData/VehicleData.kt
@@ -749,6 +749,7 @@ class VehicleData {
             mapOf("id" to "89087", "parkId" to "239087", "brand" to "MercedesBenz", "model" to "MercedesBenz Citaro C2 HYB", "type" to "BUS", "operator" to "Keolis Gironde"),
             mapOf("id" to "89091", "parkId" to "239091", "brand" to "MercedesBenz", "model" to "MercedesBenz Citaro C2 HYB", "type" to "BUS", "operator" to "Keolis Gironde"),
             mapOf("id" to "89094", "parkId" to "239094", "brand" to "MercedesBenz", "model" to "MercedesBenz Citaro C2 HYB", "type" to "BUS", "operator" to "Keolis Gironde"),
+            mapOf("id" to "89105", "parkId" to "239105", "brand" to "MercedesBenz", "model" to "MercedesBenz Citaro C2 HYB", "type" to "BUS", "operator" to "Keolis Gironde"),
 
             //MercedesBenz Citaro K C2 HYB
             mapOf("id" to "87022", "parkId" to "237022", "brand" to "MercedesBenz", "model" to "MercedesBenz Citaro K C2 HYB", "type" to "BUS", "operator" to "Keolis Gironde"),
