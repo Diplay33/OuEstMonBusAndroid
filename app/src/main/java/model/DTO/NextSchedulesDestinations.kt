87515bff5f42c49df3b4647a251cef4bbdd5e7de
Jacques HU <jacques33185@gmail.com>
2023-10-16 23:24:27 +0200

    - now trims raw string when retrieving next schedule destination data

--- app/src/main/java/model/DTO/NextSchedulesDestinations.kt
+++ app/src/main/java/model/DTO/NextSchedulesDestinations.kt
@@ -5,7 +5,7 @@ import model.DAO.AccessData.NextSchedulesDestinationData
 class NextSchedulesDestinations {
     companion object {
         fun getDestinationFromRaw(lineId: Int, destination: String): List<String> {
-            NextSchedulesDestinationData.destinations[destination]?.let { values ->
+            NextSchedulesDestinationData.destinations[destination.trim()]?.let { values ->
                 when {
                     lineId == 1 && destination == "René Coty" -> return listOf("MERIGNAC", "René Coty")
                     else -> return values
