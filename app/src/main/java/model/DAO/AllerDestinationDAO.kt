6bc1bb64bf1e0541871d39c71e37447578a39e6b
Jacques HU <jacques33185@gmail.com>
2024-06-06 23:46:20 +0200

    - added aller destination retrieving process

--- app/src/main/java/model/DAO/AllerDestinationDAO.kt
+++ app/src/main/java/model/DAO/AllerDestinationDAO.kt
@@ -1,7 +1,11 @@
 package model.DAO
 
 import androidx.room.Dao
+import androidx.room.Query
+import model.DTO.AllerDestination
 
 @Dao
 interface AllerDestinationDAO {
+    @Query("SELECT * FROM AllerDestination WHERE input = (:lineId)")
+    fun getAllerDestination(lineId: Int): AllerDestination?
 }
--- app/src/main/java/model/DTO/AllerDestinations.kt
+++ app/src/main/java/model/DTO/AllerDestinations.kt
@@ -1,9 +1,18 @@
 package model.DTO
 
 import com.diplay.ouestmonbus.MainApplication
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class AllerDestinations {
     companion object {
         private val allerDestinationDAO = MainApplication.appDatabase.getAllerDestinationDAO()
+
+        fun getAllerDestination(lineId: Int, callback: (AllerDestination?) -> Unit) {
+            CoroutineScope(Dispatchers.IO).launch {
+                callback(allerDestinationDAO.getAllerDestination(lineId))
+            }
+        }
     }
 }
