9c3dfeb55e54ca98ee697f3e90dfd0a10a691d69
Jacques HU <jacques33185@gmail.com>
2024-06-07 23:01:00 +0200

    - added retour destination retrieving process

--- app/src/main/java/model/DAO/RetourDestinationDAO.kt
+++ app/src/main/java/model/DAO/RetourDestinationDAO.kt
@@ -1,7 +1,11 @@
 package model.DAO
 
 import androidx.room.Dao
+import androidx.room.Query
+import model.DTO.RetourDestination
 
 @Dao
 interface RetourDestinationDAO {
+    @Query("SELECT * FROM RetourDestination WHERE input = (:lineId)")
+    fun getRetourDestination(lineId: Int): RetourDestination?
 }
--- app/src/main/java/model/DTO/RetourDestinations.kt
+++ app/src/main/java/model/DTO/RetourDestinations.kt
@@ -1,9 +1,32 @@
 package model.DTO
 
 import com.diplay.ouestmonbus.MainApplication
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RetourDestinations {
     companion object {
         private val retourDestinationDAO = MainApplication.appDatabase.getRetourDestinationDAO()
+
+        fun getListOfDestinations(lineId: Int, callback: (List<List<String>>) -> Unit) {
+            CoroutineScope(Dispatchers.IO).launch {
+                retourDestinationDAO.getRetourDestination(lineId)?.let { retourDestination ->
+                    val destinationsToReturn: MutableList<MutableList<String>> = mutableListOf(
+                        mutableListOf(retourDestination.city1, retourDestination.destination1)
+                    )
+
+                    retourDestination.city2?.let { destinationsToReturn.add(mutableListOf(it)) }
+                    retourDestination.destination2?.let { destinationsToReturn[1].add(it) }
+
+                    retourDestination.city3?.let { destinationsToReturn.add(mutableListOf(it)) }
+                    retourDestination.destination3?.let { destinationsToReturn[2].add(it) }
+
+                    callback(destinationsToReturn)
+                    return@launch
+                }
+                callback(listOf())
+            }
+        }
     }
 }
