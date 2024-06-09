6d51d2ce7e0268c6e6270d6ac15707f39df8d516
Jacques HU <jacques33185@gmail.com>
2024-06-09 19:02:36 +0200

    - added path destination retrieving process

--- app/src/main/java/model/DAO/PathDestinationDAO.kt
+++ app/src/main/java/model/DAO/PathDestinationDAO.kt
@@ -1,7 +1,14 @@
 package model.DAO
 
 import androidx.room.Dao
+import androidx.room.Query
+import model.DTO.PathDestination
 
 @Dao
 interface PathDestinationDAO {
+    @Query("SELECT * FROM PathDestination WHERE input = (:pathName) AND relatedLine = (:lineId)")
+    fun getLineRelatedDestination(pathName: String, lineId: Int): PathDestination?
+
+    @Query("SELECT * FROM PathDestination WHERE input = (:pathName) AND relatedLine IS NULL")
+    fun getDestination(pathName: String): PathDestination?
 }
--- app/src/main/java/model/DTO/PathDestinationsR.kt
+++ app/src/main/java/model/DTO/PathDestinationsR.kt
@@ -1,9 +1,34 @@
 package model.DTO
 
 import com.diplay.ouestmonbus.MainApplication
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class PathDestinationsR {
     companion object {
         private val pathDestinationDAO = MainApplication.appDatabase.getPathDestinationDAO()
+
+        fun getDestination(pathName: String, lineId: Int, callback: (PathDestination?) -> Unit) {
+            CoroutineScope(Dispatchers.IO).launch {
+                val reversedName = pathName.reversed()
+                var processedString = ""
+                for(i in reversedName.indices) {
+                    processedString += reversedName[i]
+                    if(reversedName[i] == ' ' && reversedName[i + 1] == '-') { break }
+                }
+                val finalString = processedString.trim().reversed()
+
+                pathDestinationDAO.getLineRelatedDestination(finalString, lineId)?.let {
+                    callback(it)
+                    return@launch
+                }
+                pathDestinationDAO.getDestination(finalString)?.let {
+                    callback(it)
+                    return@launch
+                }
+                callback(null)
+            }
+        }
     }
 }
