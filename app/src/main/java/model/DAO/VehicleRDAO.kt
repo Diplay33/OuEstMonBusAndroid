7f48806105941dddd62749afc91025038d844261
Jacques HU <jacques33185@gmail.com>
2024-06-13 21:25:07 +0200

    - added vehicle retrieving process

--- app/src/main/java/model/DAO/VehicleRDAO.kt
+++ app/src/main/java/model/DAO/VehicleRDAO.kt
@@ -1,7 +1,11 @@
 package model.DAO
 
 import androidx.room.Dao
+import androidx.room.Query
+import model.DTO.VehicleR
 
 @Dao
 interface VehicleRDAO {
+    @Query("SELECT * FROM VehicleR WHERE id = (:id)")
+    fun getVehicle(id: String): VehicleR?
 }
--- app/src/main/java/model/DTO/VehiclesR.kt
+++ app/src/main/java/model/DTO/VehiclesR.kt
@@ -1,9 +1,31 @@
 package model.DTO
 
 import com.diplay.ouestmonbus.MainApplication
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class VehiclesR {
     companion object {
         private val vehicleDAO = MainApplication.appDatabase.getVehicleDAO()
+
+        fun getVehicle(id: String, callback: (VehicleR) -> Unit) {
+            CoroutineScope(Dispatchers.IO).launch {
+                callback(vehicleDAO.getVehicle(id) ?: getUnknowVehicle(id))
+            }
+        }
+        
+        fun getUnknowVehicle(id: String): VehicleR {
+            return VehicleR(
+                id = id,
+                network = "",
+                parkId = id,
+                brand = "",
+                model = "Modèle de véhicule inconnu",
+                type = "UNKNOWN",
+                operator = "Inconnu",
+                tciId = null
+            )
+        }
     }
 }
