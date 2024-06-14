1ea2656b40c81296ef33c7be722ddbad7c1bfc5d
Jacques HU <jacques33185@gmail.com>
2024-06-14 20:46:19 +0200

    - decomposed retrieving process

--- app/src/main/java/model/SupabaseManager.kt
+++ app/src/main/java/model/SupabaseManager.kt
@@ -22,42 +22,64 @@ class SupabaseManager {
         private val supabase = MainApplication.supabase
 
         suspend fun beginSyncDatabaseProcess(callback: (Boolean) -> Unit) {
+            retrieveLines()
+            retrieveDestinations()
+            retrieveAllerDestinations()
+            retrieveRetourDestinations()
+            retrieveNextSchedulesDestinations()
+            retrievePathDestinations()
+            retrieveVehicles()
+        }
+
+        private suspend fun retrieveLines() {
             val lines = supabase
                 .from("lines")
                 .select()
                 .decodeList<Line>()
             Lines.insertLines(lines)
+        }
 
+        private suspend fun retrieveDestinations() {
             val destinations = supabase
                 .from("destinations")
                 .select()
                 .decodeList<Destination>()
             Destinations.insertDestinations(destinations)
+        }
 
+        private suspend fun retrieveAllerDestinations() {
             val allerDestinations = supabase
                 .from("aller_destinations")
                 .select()
                 .decodeList<AllerDestination>()
             AllerDestinations.insertAllerDestinations(allerDestinations)
+        }
 
+        private suspend fun retrieveRetourDestinations() {
             val retourDestinations = supabase
                 .from("retour_destinations")
                 .select()
                 .decodeList<RetourDestination>()
             RetourDestinations.insertRetourDestinations(retourDestinations)
+        }
 
+        private suspend fun retrieveNextSchedulesDestinations() {
             val nextSchedulesDestinations = supabase
                 .from("next_schedules_destinations")
                 .select()
                 .decodeList<NextSchedulesDestination>()
             NextSchedulesDestinations.insertNextSchedulesDestinations(nextSchedulesDestinations)
+        }
 
+        private suspend fun retrievePathDestinations() {
             val pathDestinations = supabase
                 .from("path_destinations")
                 .select()
                 .decodeList<PathDestination>()
             PathDestinations.insertPathDestinations(pathDestinations)
+        }
 
+        private suspend fun retrieveVehicles() {
             val vehicles = supabase
                 .from("vehicles")
                 .select()
