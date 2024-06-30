305ffa71526861e18649ed8493f95e5d28fb42a9
Jacques HU <jacques33185@gmail.com>
2024-06-30 19:05:50 +0200

    - visually improved back button into network picker

--- app/src/main/java/view/whats_new_view/network_picker/NetworkPickerTopBar.kt
+++ app/src/main/java/view/whats_new_view/network_picker/NetworkPickerTopBar.kt
@@ -3,8 +3,10 @@ package view.whats_new_view.network_picker
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
@@ -13,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @Composable
@@ -23,15 +26,22 @@ fun NetworkPickerTopBar(navController: NavController) {
         backgroundColor = if (colorScheme) Color.White else Color.Black,
         elevation = 0.dp
     ) {
-        Row {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+            .clickable { navController.navigateUp() }
+        ) {
             Icon(
                 imageVector = Icons.Rounded.ArrowBack,
                 contentDescription = null,
-                tint = if (colorScheme) Color.Black else Color.White,
+                tint = Color(android.graphics.Color.parseColor("#007AFF")),
                 modifier = Modifier
-                    .clickable { navController.navigateUp() }
                     .size(30.dp)
                     .align(Alignment.CenterVertically)
+            )
+
+            Text(
+                text = "Retour",
+                fontSize = 18.sp,
+                color = Color(android.graphics.Color.parseColor("#007AFF"))
             )
         }
     }
