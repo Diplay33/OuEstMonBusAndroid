964472eb37e894cff373bf6f8b86d07a13ffe0b6
Jacques HU <jacques33185@gmail.com>
2024-07-17 23:22:56 +0200

    - corrected rennes city coordinates

--- app/src/main/java/model/DAO/access_data/NetworkData.kt
+++ app/src/main/java/model/DAO/access_data/NetworkData.kt
@@ -5,5 +5,5 @@ import model.DTO.Network
 val networks = listOf(
     Network(1, "tbm", "Transports Bordeaux Métropole", 44.838670, -0.578620, "#009CD2"),
     Network(2, "ametis", "Ametis", 49.894067, 2.295753, "#005F7D"),
-    Network(3, "star", "STAR", 48.083328, -1.68333, "#63FD9A")
+    Network(3, "star", "STAR", 48.117266, -1.6777926, "#63FD9A")
 )
