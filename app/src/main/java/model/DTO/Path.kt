a273ed588dbc876734b6d76312ffa4f2e5cdae11
Jacques HU <jacqueshu@mac-1.home>
2024-11-20 23:37:57 +0100

    - better implementation for previous crash fix

--- app/src/main/java/model/DTO/Path.kt
+++ app/src/main/java/model/DTO/Path.kt
@@ -1,8 +1,9 @@
 package model.DTO
 
-import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 class Path(
     val id: Int,
     val name: String,
@@ -24,28 +25,5 @@ class Path(
         }
 
         return destinationName.trim()
-    }
-
-    constructor(parcel: Parcel) : this(
-        id = parcel.readInt(),
-        name = parcel.readString() ?: "",
-        direction = parcel.readString() ?: "",
-        coordinates = mutableListOf<List<List<Double>>>().apply {
-            parcel.readList(this as List<*>, List::class.java.classLoader)
-        }
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(id)
-        parcel.writeString(name)
-        parcel.writeString(direction)
-        parcel.writeList(coordinates)
-    }
-
-    override fun describeContents(): Int = 0
-
-    companion object CREATOR : Parcelable.Creator<Path> {
-        override fun createFromParcel(parcel: Parcel): Path = Path(parcel)
-        override fun newArray(size: Int): Array<Path?> = arrayOfNulls(size)
     }
 }
--- app/src/main/java/model/DTO/Vehicle.kt
+++ app/src/main/java/model/DTO/Vehicle.kt
@@ -1,12 +1,13 @@
 package model.DTO
 
-import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.Ignore
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+@Parcelize
 @Serializable
 @Entity(primaryKeys = ["id", "network"])
 data class Vehicle (
@@ -20,33 +21,4 @@ data class Vehicle (
     @SerialName("tci_id") var tciId: Int?
 ) : Parcelable {
     @Ignore val fullName: String = "$brand $model".trim()
-
-    constructor(parcel: Parcel) : this(
-        id = parcel.readString() ?: "",
-        network = parcel.readString() ?: "",
-        parkId = parcel.readString() ?: "",
-        brand = parcel.readString() ?: "",
-        model = parcel.readString() ?: "",
-        type = parcel.readString() ?: "",
-        operator = parcel.readString() ?: "",
-        tciId = parcel.readValue(Int::class.java.classLoader) as? Int
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeString(network)
-        parcel.writeString(parkId)
-        parcel.writeString(brand)
-        parcel.writeString(model)
-        parcel.writeString(type)
-        parcel.writeString(operator)
-        parcel.writeValue(tciId)
-    }
-
-    override fun describeContents(): Int = 0
-
-    companion object CREATOR : Parcelable.Creator<Vehicle> {
-        override fun createFromParcel(parcel: Parcel): Vehicle = Vehicle(parcel)
-        override fun newArray(size: Int): Array<Vehicle?> = arrayOfNulls(size)
-    }
 }
