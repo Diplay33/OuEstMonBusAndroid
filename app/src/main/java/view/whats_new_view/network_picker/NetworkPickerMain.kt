970d878483fae45d08cb30b8db15bf19d6695a91
Jacques HU <jacques.hu@laddition.com>
2025-07-23 13:58:51 +0200

    feat(networks): finish visually network picker search bar JIR-46

--- app/src/main/java/view/whats_new_view/network_picker/NetworkPickerMain.kt
+++ app/src/main/java/view/whats_new_view/network_picker/NetworkPickerMain.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,13 +13,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -33,6 +38,7 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -97,22 +103,44 @@ fun NetworkPickerMain(
                     .size(25.dp)
                 )
 
-                Row(
-                    horizontalArrangement = Arrangement.Center,
+                Surface(
+                    color = if (colorScheme) colorResource(id = R.color.light_grey) else Color.DarkGray,
                     modifier = Modifier
                         .fillMaxWidth()
+                        .padding(horizontal = 15.dp)
+                        .clip(RoundedCornerShape(15.dp))
+                        .height(50.dp)
                 ) {
-                    Surface(
-                        color = if (colorScheme) colorResource(id = R.color.light_grey) else Color.DarkGray,
+                    Box(
+                        contentAlignment = Alignment.CenterStart,
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxHeight()
                             .padding(horizontal = 15.dp)
-                            .clip(RoundedCornerShape(15.dp))
-                            .height(50.dp)
                     ) {
+                        if(searchString.value.isEmpty()) {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Icon(
+                                    imageVector = Icons.Rounded.Search,
+                                    contentDescription = null,
+                                    tint = if (colorScheme) Color.Gray else Color.White
+                                )
+
+                                Spacer(modifier = Modifier
+                                    .width(10.dp)
+                                )
+
+                                Text(
+                                    text = "Rechercher un réseau",
+                                    color = if (colorScheme) Color(0xff757575) else Color.White,
+                                    fontSize = 18.sp
+                                )
+                            }
+                        }
+
                         BasicTextField(
                             value = searchString.value,
-                            onValueChange = { searchString.value += it }
+                            onValueChange = { searchString.value = it },
+                            textStyle = TextStyle(fontSize = 18.sp)
                         )
                     }
                 }
